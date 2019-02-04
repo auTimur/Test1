275d1b2d5aa053df6a4fc613eb633b84c0e3c4b9
auTimur <azizbekuulut@gmail.com>
2019-02-04 21:23:23 +0600

    finish

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,14 @@ public class Main {
     public static void main(String[] args) {
 
 //        git init создание локального git репозиотрия
-//        git status печатеть текущее изменение в проекте
-//        git add проиндексировать все изменения
+// !       git status печатеть текущее изменение в проекте
+// !       git add проиндексировать все изменения
 //        git commit создать комент
-//        git commit -m "Second commit"
+// !       git commit -m "Second commit"
 //        git config --global user.email "azizbekuulut@gmail.com"
 //        git config --global user.name "auTimur"
 //        VIM commands
-//        выход:
-//        Esc;
-//        Shift + :;
-//        q !; Enter;
-//        изменить файл:i
-//        сохранить и выйти ?:
-//        Esc Shift + :wq Enter
+//        выход:      Esc;     Shift + :;   q !; Enter;     изменить файл:i    сохранить и выйти ?:     Esc Shift + :wq Enter
 //        Пробую сам
 
     }
