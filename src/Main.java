da7580422af6c792c95ce803f462a48eee09ef67
auTimur <azizbekuulut@gmail.com>
2019-02-04 20:23:18 +0600

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,15 @@ public class Main {
 //        git add проиндексировать все изменения
 //        git commit
 //        git config --global user.email "azizbekuulut@gmail.com"
+//        git config --global user.name "auTimur"
+//        VIM commands
+//        выход:
+//        Esc;
+//        Shift + :;
+//        q !; Enter;
+//        изменить файл:i
+//        сохранить и выйти ?:
+//        Esc Shift + :wq Enter
+
     }
 }
