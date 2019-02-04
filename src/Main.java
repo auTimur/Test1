0c050654acfe52c01f5bccfb06262f60c79b33d2
auTimur <azizbekuulut@gmail.com>
2019-02-04 20:14:15 +0600

    First commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+//        git init создание локального git репозиотрия
+//        git status печатеть текущее изменение в проекте
+//        git add проиндексировать все изменения
+//        git commit
+//        git config --global user.email "azizbekuulut@gmail.com"
+    }
+}
